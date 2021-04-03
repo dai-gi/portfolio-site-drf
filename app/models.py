c8b983a1b096c0bb67861eebacc1d4a04578bbff
dai-gi <daisuke.u524@gmail.com>
2021-04-03 20:59:14 +0900

    プロフィールモデルを作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    title = models.CharField('タイトル', max_length=100, null=True, blank=True)
+    subtitle = models.CharField('サブタイトル', max_length=100, null=True, blank=True)
+    name = models.CharField('名前', max_length=100)
+    introduction = models.TextField('自己紹介')
+    programming = models.CharField('学習言語', max_length=100)
+    github = models.CharField('github', max_length=100, null=True, blank=True)
+    twitter = models.CharField('twitter', max_length=100, null=True, blank=True)
+    linkedin = models.CharField('linkedin', max_length=100, null=True, blank=True)
+    facebook = models.CharField('facebook', max_length=100, null=True, blank=True)
+    instagram = models.CharField('instagram', max_length=100, null=True, blank=True)
+    topimage = models.ImageField(upload_to='images', verbose_name='トップ画像')
+    subimage = models.ImageField(upload_to='images', verbose_name='サブ画像')
+
+    def __str__(self):
+        return self.name
