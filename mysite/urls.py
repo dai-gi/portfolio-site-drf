1231c5457e2356d72aa00a7413cba23756573851
dai-gi <daisuke.u524@gmail.com>
2021-04-29 21:09:30 +0900

    apiに変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('api/', include('app.urls')),
 ]
 
 if settings.DEBUG:
