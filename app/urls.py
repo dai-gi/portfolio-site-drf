9220dd4805959615d358d506d51f695db52d4d63
dai-gi <daisuke.u524@gmail.com>
2021-04-29 21:12:16 +0900

    profile, portfolio-detailを追加

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('portfolio/<str:pk>', views.PortfolioDtailView.as_view(), name='portfolio-detail'),
 ]
