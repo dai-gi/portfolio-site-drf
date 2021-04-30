b8f2d769c0be90ad7dae20d37fae4a8d3b7c17da
dai-gi <daisuke.u524@gmail.com>
2021-05-01 05:48:35 +0900

    ポートフォリオの詳細URLを削除

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,4 @@ from app import views
 
 urlpatterns = [
     path('profile', views.ProfileView.as_view(), name='profile'),
-    path('portfolio/<str:pk>', views.PortfolioDtailView.as_view(), name='portfolio-detail'),
 ]
