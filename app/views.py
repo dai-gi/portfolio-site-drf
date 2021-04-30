1309486a9d71a491d02365654e60eb93ddd656a4
dai-gi <daisuke.u524@gmail.com>
2021-05-01 05:47:55 +0900

    プロフィールビューに変更

--- app/views.py
+++ app/views.py
@@ -1,13 +1,8 @@
-from django.views.generic import View
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PostSerializer
 from .models import Profile
 
 
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        profile_data = Profile.objects.all()
-        if profile_data.exists():
-            profile_data = profile_data.order_by("-id")[0]
-        return render(request, 'app/index.html', {
-            'profile_data': profile_data,
-        })
+class ProfileView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializers = ProfileSerializer
