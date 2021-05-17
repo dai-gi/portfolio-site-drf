2056ad1f27cef176aabc47fbc1ae12c0022a54f2
dai-gi <daisuke.u524@gmail.com>
2021-05-18 05:28:29 +0900

    introduction項目を削除

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class Profile(models.Model):
     twitter = models.CharField('twitter', max_length=100, null=True, blank=True)
     github = models.CharField('github', max_length=100, null=True, blank=True)
     programming = models.CharField('学習言語', max_length=100)
-    introduction = models.TextField('自己紹介')
     topimage = models.ImageField(upload_to='images', verbose_name='トップ画像')
     subimage = models.ImageField(upload_to='images', verbose_name='サブ画像')
 
