8c2c7fc0411f8702a7110c53b46b41b0a238700f
dai-gi <daisuke.u524@gmail.com>
2021-05-19 05:56:43 +0900

    ProductionView, ProductionDtailView追加

--- app/views.py
+++ app/views.py
@@ -1,8 +1,18 @@
 from rest_framework import generics
-from .serializers import ProfileSerializer
-from .models import Profile
+from .serializers import ProfileSerializer, ProductionSerializer
+from .models import Profile, Production
 
 
 class ProfileView(generics.ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProductionView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProductionSerializer
+
+
+class ProductionDetailView(generics.RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProductionSerializer
