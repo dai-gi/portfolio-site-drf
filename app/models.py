aa1de0ad12caf3025ec3cb56e9af72e523d58686
dai-gi <daisuke.u524@gmail.com>
2021-05-19 05:33:33 +0900

    productionモデルを追加

--- app/models.py
+++ app/models.py
@@ -12,3 +12,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Production(models.Model):
+    title = models.CharField('タイトル', max_length=100)
+    image = models.ImageField(upload_to='images', verbose_name='イメージ画像')
+    thumbnail = models.ImageField(upload_to='images', verbose_name='サムネイル')
+    skill = models.CharField('スキル', max_length=100)
+    github = models.CharField('github', max_length=100, null=True, blank=True)
+    url = models.CharField('URL', max_length=100, null=True, blank=True)
+    created = models.DateField('作成日時')
+    description = models.TextField('説明')
+
+    def __str__(self):
+        return self.title
