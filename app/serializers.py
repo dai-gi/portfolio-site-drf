90f28a7b660764f3cc8751caee6b4b83acf2689b
dai-gi <daisuke.u524@gmail.com>
2021-05-19 05:43:55 +0900

    introduction削除

--- app/serializers.py
+++ app/serializers.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('id', 'title', 'subtitle', 'name', 'twitter', 'github', 'programming', 'introduction', 'topimage', 'subimage')
+        fields = ('id', 'title', 'subtitle', 'name', 'twitter', 'github', 'programming', 'topimage', 'subimage')
