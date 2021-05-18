bcd19fa0a8ca140438d181320935acd86b12a5c5
dai-gi <daisuke.u524@gmail.com>
2021-05-19 05:50:32 +0900

    ProductionSerializer追加

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Production
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id', 'title', 'subtitle', 'name', 'twitter', 'github', 'programming', 'topimage', 'subimage')
+
+
+class ProductionSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")
+    class Meta:
+        model = Production
+        fields = ('id', 'title', 'image', 'thumbnail', 'skill', 'github', 'url', 'created', 'description')
