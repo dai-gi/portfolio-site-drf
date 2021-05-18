74a717550d32ef24b873a26ecae415971a8a1225
dai-gi <daisuke.u524@gmail.com>
2021-05-19 05:43:16 +0900

    productionURLを追加

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('production/', views.ProductionView.as_view(), name='production'),
+    path('production/<str:pk>', views.ProductionDetailView.as_view(), name='production-detail'),
 ]
