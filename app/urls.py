24085bcad973b584caa26537269b145d07630320
dai-gi <daisuke.u524@gmail.com>
2021-05-23 17:52:02 +0900

    修正

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from app import views
 urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('production/', views.ProductionView.as_view(), name='production'),
-    path('production/<str:pk>', views.ProductionDetailView.as_view(), name='production-detail'),
+    path('production/<str:pk>/', views.ProductionDetailView.as_view(), name='production-detail'),
 ]
